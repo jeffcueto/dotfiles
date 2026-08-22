3d89761a98941671b54e0e2428610c0468516957
Jeff Cueto Rivera <jeff@jfcor.com>
2026-08-21 19:56:55 -0500

    Now testing alias ls='eza --icons=alsways'

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,5 +1,7 @@
 # This is for move to Miunidad
 alias m='cd $HOME/Miunidad/'
 
+# Alias eza with ls
+alias ls="eza --icons=always"
 
 alias config='/usr/bin/git --git-dir=/home/jeff/.dotfiles/ --work-tree=/home/jeff'
