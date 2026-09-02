b493d8c99f6c31eb589b1e7b169e095600a8dd91
Jeff Cueto Rivera <jeff@jfcor.com>
2026-09-01 20:14:33 -0500

    Adding group parameter to alias eza

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -2,6 +2,6 @@
 alias m='cd $HOME/Miunidad/'
 
 # Alias eza with ls
-alias ls="eza --icons=always"
+alias ls="eza --icons=always --group"
 
 alias config='/usr/bin/git --git-dir=/home/jeff/.dotfiles/ --work-tree=/home/jeff'
